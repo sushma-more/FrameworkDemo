de6d1ccbfc0f79bebbb44123dec305f850e241ee
EETADMIN <eetadmin@Sushma-More-Kadam.local>
2020-01-16 16:59:48 +0530

    first commit

---
+++ FrameworkDemo/BaseClass.swift
@@ -0,0 +1,15 @@
+//
+//  BaseClass.swift
+//  FrameworkDemo
+//
+//  Created by EETADMIN on 16/01/20.
+//  Copyright © 2020 EETADMIN. All rights reserved.
+//
+
+import Foundation
+
+public class BaseClass{
+    public func callAPI(){
+        print("baseclass api function")
+    }
+}
---
+++ FrameworkDemo/FrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkDemo.h
+//  FrameworkDemo
+//
+//  Created by EETADMIN on 16/01/20.
+//  Copyright © 2020 EETADMIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkDemo.
+FOUNDATION_EXPORT double FrameworkDemoVersionNumber;
+
+//! Project version string for FrameworkDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkDemo/PublicHeader.h>
+
+
